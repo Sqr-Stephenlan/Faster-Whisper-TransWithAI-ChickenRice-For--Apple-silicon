bd8c5e520d95df56546169e2ab154696b964d518
Sqr-Stephenlan <133780413@qq.com>
2026-07-15 10:25:35 +0800

    build: regenerate macOS app for subtitle format tags

--- scripts/build_macos_app.sh
+++ scripts/build_macos_app.sh
@@ -72,9 +72,9 @@ cat >"$PLIST" <<'PLIST'
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.0.0</string>
+    <string>1.1.0</string>
     <key>CFBundleVersion</key>
-    <string>1</string>
+    <string>2</string>
     <key>LSMinimumSystemVersion</key>
     <string>13.0</string>
     <key>NSAppleEventsUsageDescription</key>
